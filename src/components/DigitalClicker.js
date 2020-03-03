12981bd025b3cde446a435e1ff5acc99cad99fc2
mmelihdogan <doganmmelih@gmail.com>
2020-03-03 18:40:23 +0300

    Done.

--- src/components/DigitalClicker.js
+++ src/components/DigitalClicker.js
@@ -1,1 +1,25 @@
 // Code DigitalClicker Component Here
+
+import React, { Component } from 'react';
+
+class DigitalClicker extends Component {
+    constructor() {
+        super()
+        this.state = {
+            timesClicked: 0
+        }
+    }
+
+    clicked = () => {
+        this.setState({
+            timesClicked: this.state.timesClicked + 1
+          })
+    }
+  
+  render() {
+    return (
+    <button onClick={this.clicked}>{this.state.timesClicked}</button>
+    )
+  }
+}
+export default DigitalClicker; 
--- src/components/YouTubeDebugger.js
+++ src/components/YouTubeDebugger.js
@@ -1,1 +1,12 @@
 // Code YouTubeDebugger Component Here
+
+import React, { Component } from 'react';
+
+class YouTubeDebugger extends Component {  
+    render() {
+    return (
+        <div></div>
+    )
+  }
+}
+export default YouTubeDebugger; 
