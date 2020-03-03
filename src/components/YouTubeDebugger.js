9ea1a7b6da24a9d727f8fc4ef5281c784d9e7ed0
mmelihdogan <doganmmelih@gmail.com>
2020-03-03 19:00:19 +0300

    Done.

--- src/components/YouTubeDebugger.js
+++ src/components/YouTubeDebugger.js
@@ -3,9 +3,49 @@
 import React, { Component } from 'react';
 
 class YouTubeDebugger extends Component {  
+    constructor() {
+        super()
+        this.state = {
+            errors: [],
+            user: null,
+            settings: {
+              bitrate: 8,
+              video: {
+                resolution: '1080p'
+              }
+            }
+        }
+    }
+
+
+    bit = () => {
+        // Used spread operator instead of Object.assign() method!
+        this.setState({
+            settings: { // object that we want to update
+                ...this.state.settings, // keep all other key-value pairs
+                bitrate: 12 // update this
+            }
+        })
+    }
+
+    res = () => {
+        // Used Object.assign() method
+
+        this.setState({
+            settings: Object.assign({}, this.state.settings, {video: {resolution: '720p'}})
+        })
+
+    }
+
+    /** SOURCE: https://stackoverflow.com/questions/43638938/updating-an-object-with-setstate-in-react */
+
+
     render() {
     return (
-        <div></div>
+        <div>
+        <button className="bitrate" onClick={this.bit}>Bitrate: {this.state.settings.bitrate}</button>
+        <button className="resolution" onClick={this.res}>Resolution:</button>
+        </div>
     )
   }
 }
