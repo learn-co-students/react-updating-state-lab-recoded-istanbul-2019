e95a47addcfd1b2954b6538de5098bb380f40502
emrerdem1 <emrerdem94@gmail.com>
2020-05-26 03:41:48 +0300

    Done.

--- src/components/DigitalClicker.js
+++ src/components/DigitalClicker.js
@@ -1,1 +1,24 @@
-// Code DigitalClicker Component Here
+import React from 'react';
+
+export default class DigitalClicker extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            timesClicked: 0
+        }
+    }
+    counter = () => this.setState(prevState => {
+        return {
+            timesClicked: prevState.timesClicked + 1
+        }
+    })
+    render() {
+        const { timesClicked } = this.state;
+        return (
+            <React.Fragment>
+                <label for={timesClicked}></label>
+                <button id={timesClicked} value={timesClicked} onClick={this.counter}>{timesClicked}</button>
+            </React.Fragment>
+        )
+    }
+}
--- src/components/YouTubeDebugger.js
+++ src/components/YouTubeDebugger.js
@@ -1,1 +1,49 @@
-// Code YouTubeDebugger Component Here
+import React from 'react';
+
+export default class YouTubeDebugger extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            errors: [],
+            user: null,
+            settings: {
+                bitrate: 8,
+                video: {
+                    resolution: '1080p'
+                }
+            }
+        }
+    }
+    setBitrate = () => this.setState({
+        settings: {
+            ...this.state.settings,
+            bitrate: 12
+        }
+    });
+    setResolution = () => this.setState({
+        settings: {
+            ...this.state.settings,
+            video: {
+                resolution: '720p'
+            }
+        }
+    });
+    /*setBitrate = () => this.setState({
+        ...this.state.settings,
+        bitrate: 12
+
+    });
+    setResolution = () => this.setState({
+        ...this.state.settings,
+        resolution: '720p'
+    });*/
+    render() {
+        //const { bitrate, video: { resolution } } = this.state.settings;
+        return (
+            <React.Fragment>
+                <button className="bitrate" onClick={this.setBitrate}></button>
+                <button className="resolution" onClick={this.setResolution}></button>
+            </React.Fragment>
+        )
+    }
+}
