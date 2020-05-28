dc9bc41614fb8abe4a2958b57e77a2e7ad8bf10f
tugba <ttugbasen@gmail.com>
2020-05-28 14:30:49 +0300

    Done.

--- src/components/DigitalClicker.js
+++ src/components/DigitalClicker.js
@@ -1,1 +1,35 @@
-// Code DigitalClicker Component Here
+import React, { Component } from "react"
+
+class DigitalClicker extends Component {
+    constructor() {
+        super()
+
+        this.state = {
+            timesClicked: 0
+        }
+    }
+
+    handleClick = () => {
+        this.setState(previousState => {
+            return {
+                timesClicked: previousState.timesClicked + 1
+            }
+        })
+    }
+
+        render() {
+            return(
+                <div>
+                <button onClick={this.handleClick}>
+                <label>{this.state.timesClicked}</label>
+                </button>
+                </div>
+            )
+
+
+
+        }
+    
+}
+
+export default DigitalClicker
--- src/components/YouTubeDebugger.js
+++ src/components/YouTubeDebugger.js
@@ -1,1 +1,49 @@
-// Code YouTubeDebugger Component Here
+import React, { Component } from "react";
+
+class YouTubeDebugger extends Component {
+    constructor() {
+        super()
+
+        this.state = {
+            errors: [],
+            user: null,
+            settings: {
+              bitrate: 8,
+              video: {
+                resolution: '1080p'
+              }
+            }
+          }
+    }
+
+    bitrate = () => {
+        this.setState({
+            settings: {
+                ...this.state.settings,
+                bitrate: 12,
+            }
+        })
+    }
+
+    resolution = () => {
+        this.setState({
+            settings: {
+                ...this.state.settings,
+                video: {
+                    resolution: '720p',
+                }
+            }
+        })
+    }
+
+    render() {
+        return (
+            <div>
+                <button className='bitrate' onClick={this.bitrate}>Bitrate Buttom</button>
+                <button className='resolution' onClick={this.resolution}>Resolution Button</button>
+            </div>
+        )
+    }
+}
+
+export default YouTubeDebugger
